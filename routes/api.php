ea7af317667c7dd775c1d17246085161898c6284
myomaung5255225 <myomaung5255225@gmail.com>
2022-01-09 21:40:40 +0630

    refactor

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// v1
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::middleware('api:auth')->group(function () {
